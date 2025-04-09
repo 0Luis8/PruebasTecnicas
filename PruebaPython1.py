75ca80272ada1bb3361c1a225db57fbeadd382b5
Luis de Castro Mora <luisdecmora@gmail.com>
2025-04-09 10:40:54 +0200

    Subiendo la primera prueba técnica a resolver

---
+++ PruebaPython1.py
@@ -0,0 +1,43 @@
+"""
+  1.- /*
+
+  Escribe una función que reciba un texto y retorne verdadero o
+  falso (Boolean) según sean o no palíndromos.
+  Un Palíndromo es una palabra o expresión que es igual si se lee
+  de izquierda a derecha que de derecha a izquierda.
+  NO se tienen en cuenta los espacios, signos de puntuación y tildes.
+  Ejemplo: Ana lleva al oso la avellana. # TRUE
+
+"""
+
+
+
+
+
+
+
+
+
+"""
+  2.-
+
+  Escribe una función que calcule y retorne el factorial de un número dado
+  de forma recursiva.
+
+"""
+
+
+
+
+
+
+
+"""
+  3.-
+
+  Escribe una función que calcule si un número dado es un número de Armstrong
+  (o también llamado narcisista).
+  Si no conoces qué es un número de Armstrong, debes buscar información
+  al respecto.
+
+"""
